10b20e9276645aa842b564f6982fd3a45a9e4a14
DHIREN VISHWAKARMA <69718040+Dhiren-dev@users.noreply.github.com>
2024-12-28 17:21:21 +0530

    added .get in authRoute

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post("/signup", upload.single("avatar"), auth.signUp);
 router.post("/login", auth.logIn);
 router.post("/forget-password", auth.forgotPassword);
-router.post("/check", auth.check);
+router.get("/check", auth.check);
 router.post("/reset-password/:token", auth.resetPassword);
 router.post("/logout", auth.logOut);
 
